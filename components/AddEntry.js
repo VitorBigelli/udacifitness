b5bb7450ae1b004f2eb26840ef3e22d59013e1dd
Vitor <vitorbigelli@hotmail.com>
2018-01-03 15:27:39 -0200

    feat: Create metrics basic view

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'; 
 import { View, Text} from 'react-native';
 import { getMetricMetaInfo } from '../utils/helpers'; 
+import UdaciSlider from './UdaciSlider';
+import UdaciSteppers from './UdaciSteppers';
 
 export default class AddEntry extends Component {
 
@@ -43,9 +45,35 @@ export default class AddEntry extends Component {
 	}
 
 	render() {
+		const metaInfo = getMetricMetaInfo()
+
 		return (
 			<View> 
-				{getMetricMetaInfo('bike').getIcon()}			
+				{Object.keys(metaInfo).map((key) => {
+					const { getIcon, type, ...rest } = metaInfo[key]
+					const value = this.state[key]
+
+					return (
+						<View key={key} >
+							{getIcon()}
+							{type === 'slider' 
+								?	<UdaciSlider 
+										value={value}
+										onChange={(value) => this.slide(key, value)}
+										{...rest}
+									/>
+									:
+									<UdaciSteppers 
+										value={value}
+										onIncrement={(value) => this.increment(key)}
+										onDecrement={(value) => this.decrement(key)}
+										{...rest}
+									/>
+							}
+						</View>
+
+					)
+				})}			
 			</View>
 		)
 	}
